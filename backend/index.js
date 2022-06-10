0b9466150975f6db1f8233829d86ee646857a09d
Ferenc Solyom <ifj.solyom.feco15@gmail.com>
2022-06-10 07:16:25 +0200

    init

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname+"/page.html");
 });
 
 app.post("/", function(req, res){
-  res.send("<h2>nope</h2>");
+  var n1 = Number(req.body.num1);
+  var n2 = Number(req.body.num2);
+  var result = n1+n2 ;
+  res.send(""+result);
 });
 
-app.listen(3000, () => console.log('example'));
+app.listen(3000, () => console.log('Server is running at: 3000'));
