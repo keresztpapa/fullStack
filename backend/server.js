c0455e5e0f2a77dc515414ddf588eb39fe9a5c80
Ferenc Solyom <ifj.solyom.feco15@gmail.com>
2022-06-11 13:28:43 +0200

    steps

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname+"/page.html");
-});
-
-app.post("/", function(req, res){
-  var n1 = Number(req.body.num1);
-  var n2 = Number(req.body.num2);
-  var result = n1+n2 ;
-  res.send(""+result);
 });
 
 app.listen(3000, () => console.log('Server is running at: 3000'));
